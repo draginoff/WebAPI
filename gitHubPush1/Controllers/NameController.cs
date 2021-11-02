a0f224a63dfc093ca6c584b3e167119a156b27f7
draginoff <93340583+draginoff@users.noreply.github.com>
2021-11-02 13:09:17 -0400

    Addition of Name Controller

---
+++ gitHubPush1/Controllers/NameController.cs
@@ -0,0 +1,28 @@
+﻿using InClassLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InClassLab.Controllers
+{
+
+    [ApiController]
+    [Route("[Controller]")]
+    public class NameController : Controller
+    {
+
+        [HttpGet]
+
+        public NameContent Get()
+        {
+            return new NameContent
+            {
+                FirstName = "David",
+                LastName = "Draginoff"
+            };
+        }
+        
+    }
+}
